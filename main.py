752f83b4c6350ad0baac44a4b01ee5e72f97bb2c
Jamie Zhao <bluewolfJ124@gmail.com>
2024-07-13 14:10:34 +1000

    New method to sample random index

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 os.system('cls')
 largest_legs = 0
 largest_arms = 0
-x,y = [],[]
-index = []
+index_list = []
 print("Welcome to Jamie's amazing data sample sampler")
 while True:
     sample_size = input("Please input the size of the random sample: ")
@@ -24,23 +23,24 @@ for i in dic:
     i["Waist to floor (cm)"] = int(i["Waist to floor (cm)"]) # Filter out some random spaces in the data
     if i["Waist to floor (cm)"] > largest_legs:
         largest_legs = i["Waist to floor (cm)"]
-sample_list = random.sample(dic, k=sample_size)
-
-for i in sample_list:
-    index.append(dic.index(i))
-    i.update({"Index": dic.index(i)})
-
+x,y = [],[]
+index_list = random.sample(range(0,500), k=sample_size)
+index_list.sort()
+sample_list = []
+for i in index_list:
+    item = dic[i]
+    item.update({"Index": i})
+    x.append(item["Shoulder to wrist (cm)"])
+    y.append(item["Waist to floor (cm)"])
+    sample_list.append(item)
+    
 df = pandas.DataFrame(sample_list)
 print(df)
 response = input("Would you like to export the data to excel? Y/N ")
 if response == "Y" or response == "y":
     df.to_excel('output.xlsx', sheet_name='sheet1', index=False)
     print(f"Results saved to: {os.path.abspath('output.xlsx')}")
-for i in sample_list:
-    x.append(i["Shoulder to wrist (cm)"])
-    y.append(i["Waist to floor (cm)"])
-for i, txt in enumerate(index):
-    plt.annotate(txt, (x[i], y[i]))
+
 plt.scatter(x, y)
 plt.xlim(0, 90)
 plt.ylim(0, 140)
